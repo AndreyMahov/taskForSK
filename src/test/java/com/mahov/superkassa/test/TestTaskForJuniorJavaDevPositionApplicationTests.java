87c4dad6dcc37a8e24775844e51fec7b18092a82
Andrey <averel7log@gmail.com>
2022-12-26 14:35:19 +0300

    added context test

--- src/test/java/com/mahov/superkassa/test/TestTaskForJuniorJavaDevPositionApplicationTests.java
+++ src/test/java/com/mahov/superkassa/test/TestTaskForJuniorJavaDevPositionApplicationTests.java
@@ -2,8 +2,12 @@ package com.mahov.superkassa.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.yaml")
 class TestTaskForJuniorJavaDevPositionApplicationTests {
 
 	@Test
